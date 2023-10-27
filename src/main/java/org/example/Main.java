ee5636f3f7c2c309fa811c1a5089f4628f00a90c
SuryaVamsiKalaga <Surya.Vamsi@iiitb.ac.in>
2023-10-27 15:20:41 +0530

    Added calculator program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,39 @@
 package org.example;
+import java.util.*;
+import org.apache.logging.log4j.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Welcome to Calculator...");
+        System.out.println("Choose your operations");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        int type = myObj.nextInt();
+        System.out.println("Enter the 1st operand");
+        int a = myObj.nextInt();
+        System.out.println("Enter the 2nd operand");
+        int b = myObj.nextInt();
+        int res = 0;
+        if(type == 1)
+        {
+            res = a+b;
+        }    
+        if(type == 2)
+        {
+            res = a-b;
         }
+        if(type == 3)
+        {
+            res = a*b;
+        }
+        if(type == 4)
+        {
+            res = a/b;
+        }
+        System.out.println("result = " + res);
     }
 }
